eb9123b90e8598cff3fce6b1b2be73e92888efe9
Sonali Marlin <sonali.marlin@techtonic.com>
2018-10-02 16:03:34 -0600

    add functions up to getRandomBook

--- js/book.js
+++ js/book.js
@@ -9,4 +9,8 @@ Book.prototype.editBook = function(oBook) {
 
 };
 
-var firstBook = new Book ("The Glass Castle", "Jeannette Walls", 288, "March 2005");
+var firstBook = new Book ("The Glass Castle", "Jeannette Walls", 288, "March 14th 2005");
+var secondBook = new Book ("Little Fires Everywhere", "Celeste Ng", 338, "September 12th 2017");
+var thirdBook = new Book ("Into the Water ", "Paula Hawkins", 386, "May 2nd 2017");
+var fourthBook = new Book ("The Radium Girls: The Dark Story of America's Shining Women", "Kate Moore", 496, "May 2nd 2017");
+var fifthBook = new Book ("Before We Were Yours", "Lisa Wingate", 342, "June 6th 2017");
--- js/engine.js
+++ js/engine.js
@@ -1,26 +1,65 @@
 function Library() {
   this.bookShelf = new Array();
-
-};
-Library.prototype.addBook = function(book){
-  this.bookShelf.push(book);
 };
 
-Library.prototype.removeBookByTitle = function(title){
-  this.bookShelf.pop(this.bookShelf.filter(function(book){
-    (book.title === title);
+//Function to add a book. If title exists, will return false
+Library.prototype.addBook = function(book) {
+  for (var i = 0; i < this.bookShelf.length; i++){
+    if(this.bookShelf[i].title === book.title){
+      return false;
+    }
   }
-))};
+  this.bookShelf.push(book);
+  return true;
+};
 
-Library.prototype.removeBookByAuthor = function(author){};
-Library.prototype.getRandomBook = function(){};
-Library.prototype.getBookByTitle = function(title){};
+//Function to remove a book by title. Will match partial words and remove all. If no match, will return false
+Library.prototype.removeBookByTitle = function(title) {
+  if(this.bookShelf.filter(item => item.title.indexOf(title) > -1).length===0){
+    return false;
+  } else {
+    this.bookShelf = this.bookShelf.filter(item => item.title.indexOf(title) === -1);
+    return true;
+  }
+};
+
+//Function to remove a book by author. Will match all authors and remove all. If no match, will return false
+Library.prototype.removeBookByAuthor = function(author) {
+  if(this.bookShelf.filter(item => item.author === author).length === 0){
+    return false;
+  } else {
+    this.bookShelf = this.bookShelf.filter(item => item.author !== author);
+    return true;
+  }
+}
+
+//Generates a random number between 0 and the length of the array and returns that array item
+Library.prototype.getRandomBook = function() {
+  if(this.bookShelf.length === 0){
+    return null;
+  }
+  var num = Math.floor(Math.random() * this.bookShelf.length);
+  return this.bookShelf[num];
+};
+
+
+Library.prototype.getBookByTitle = function(title) {};
 Library.prototype.getBookByAuthor = function(author){};
 Library.prototype.addBooks = function(books){};
-Library.prototype.getAuthors = function(){};
-Library.prototype.getRandomAuthorName = function(){};
+
+Library.prototype.getAuthors = function() {
+  var authors = this.bookShelf.map(item => item.author);
+  console.log(authors);
+  return authors;
+};
+
+Library.prototype.getRandomAuthorName = function() {};
 
 document.addEventListener("DOMContentLoaded", function(e) {
   window.gLibrary = new Library();
   gLibrary.addBook(firstBook);
+  gLibrary.addBook(secondBook);
+  gLibrary.addBook(thirdBook);
+  gLibrary.addBook(fourthBook);
+  gLibrary.addBook(fifthBook);
 });
