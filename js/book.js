9dad45887217e76641c7daf80389ce65c4d64c1e
Sonali Marlin <sonali.marlin@techtonic.com>
2018-10-03 16:28:32 -0600

    add date object change

--- js/book.js
+++ js/book.js
@@ -2,7 +2,7 @@ function Book(oBook) {
   this.title = oBook.title;
   this.author = oBook.author;
   this.numPages = oBook.numPages;
-  this.pubDate = oBook.pubDate;
+  this.pubDate = new Date(oBook.pubDate);
 };
 
 Book.prototype.editBook = function(oBook) {
@@ -10,11 +10,17 @@ Book.prototype.editBook = function(oBook) {
   this.author = oBook.author || this.author;
   this.numPages = oBook.numPages || this.numPages;
   this.pubDate = oBook.pubDate || this.pubDate;
-  return true;
+  return this;
 };
 
-var firstBook = new Book ({title: "The Glass Castle", author: "Jeannette Walls", numPages: 288, pubDate: "March 14th 2005"});
-var secondBook = new Book ({title: "Little Fires Everywhere", author: "Celeste Ng", numPages: 338, pubDate: "September 12th 2017"});
-var thirdBook = new Book ({title: "Into the Water", author: "Paula Hawkins", numPages: 386, pubDate: "May 2nd 2017"});
-var fourthBook = new Book ({title: "The Radium Girls: The Dark Story of America's Shining Women", author: "Kate Moore", numPages: 496, pubDate: "May 2nd 2017"});
-var fifthBook = new Book ({title: "Before We Were Yours", author: "Lisa Wingate", numPages: 342, pubDate: "June 6th 2017"});
+var firstBook = new Book ({title: "The Glass Castle", author: "Jeannette Walls", numPages: 288, pubDate: "2011-10-10"});
+var secondBook = new Book ({title: "Little Fires Everywhere", author: "Celeste Ng", numPages: 338, pubDate: "2017-9-12"});
+var thirdBook = new Book ({title: "Into the Water", author: "Paula Hawkins", numPages: 386, pubDate: "2017-5-2"});
+var fourthBook = new Book ({title: "The Radium Girls: The Dark Story of America's Shining Women", author: "Kate Moore", numPages: 496, pubDate: "2017, 12, 31"});
+var fifthBook = new Book ({title: "Before We Were Yours", author: "Lisa Wingate", numPages: 342, pubDate: "2017, 6, 18"});
+
+
+var date1 = Date.parse('01 Jan 1970');
+var date2 = new Date('15 Jan 2002')
+console.log(date1);
+console.log(date2);
